a64a5d7e7ea31d7926624a49ff06631c54d8bcc2
DustinZheng <konghuarukhr@163.com>
2021-02-12 21:04:11 +0800

    Create hello web server

--- web.go
+++ web.go
@@ -42,22 +42,25 @@ func main() {
 func handleDing(w http.ResponseWriter, r *http.Request) {
 	log.Printf("recv: %s\n", r.URL)
 	fmt.Fprintf(w, "recv: %s\n", r.URL)
+	handlePost2Ding(w, r)
 }
 
 func handlePost2Ding(w http.ResponseWriter, r *http.Request) {
 	url := "https://oapi.dingtalk.com/robot/send?access_token=a6b3039dc83a8911870274109dea4671286db8091f646aa44c7dd266bc0c579d"
 	contentType := "application/json"
 
-	text := make(map[string]string)
-	text["content"] = "消息: test"
-	msg := make(map[string]interface{})
-	msg["msgtype"] = "text"
-	msg["text"] = text
+	// text := make(map[string]string)
+	// text["content"] = "消息: test"
+	// msg := make(map[string]interface{})
+	// msg["msgtype"] = "text"
+	// msg["text"] = text
+	msg := makeDingActionCard("test", "![screenshot](https://img.alicdn.com/tfs/TB1NwmBEL9TBuNjy1zbXXXpepXa-2400-1218.png) \n\n #### 乔布斯 20 年前想打造的苹果咖啡厅 \n\n Apple Store 的设计正从原来满满的科技感走向生活化，而其生活化的走向其实可以追溯到 20 年前苹果一个建立咖啡馆的计划")
 
 	out, err := json.Marshal(msg)
 	if err != nil {
 		fmt.Fprintf(w, "error: %s\n", err)
 	} else {
+		log.Printf("msg: %s\n", out)
 		resp, err := http.Post(url, contentType, bytes.NewReader(out))
 		if err != nil {
 			fmt.Fprintf(w, "error: %s\n", err)
@@ -71,4 +74,37 @@ func handlePost2Ding(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}
+}
+
+type dingActionCard struct {
+	Msgtype    string     `json:"msgtype"`
+	ActionCard actionCard `json:"actionCard"`
+}
+
+type actionCard struct {
+	Title          string `json:"title"`
+	Text           string `json:"text"`
+	HideAvatar     string `json:"hideAvatar"`
+	BtnOrientation string `json:"btnOrientation"`
+	Btns           []btn  `json:"btns"`
+}
+
+type btn struct {
+	Title     string `json:"title"`
+	ActionURL string `json:"actionURL"`
+}
+
+func makeDingActionCard(title string, text string) *dingActionCard {
+	return &dingActionCard{
+		Msgtype: "actionCard",
+		ActionCard: actionCard{
+			Title:          title,
+			Text:           text,
+			HideAvatar:     "0",
+			BtnOrientation: "0",
+			Btns: []btn{
+				{Title: "标题1", ActionURL: "https://konghuarukhr.github.io"},
+				{Title: "标题1", ActionURL: "https://konghuarukhr.github.io"},
+			},
+		}}
 }
