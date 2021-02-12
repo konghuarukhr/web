851cb85910f2120f7ac23deec2c1d37e9aed25ca
DustinZheng <konghuarukhr@163.com>
2021-02-12 19:54:36 +0800

    Create hello web server

--- web.go
+++ web.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -30,11 +32,32 @@ func main() {
 	log.SetPrefix("goweb: ")
 
 	http.HandleFunc("/ding", handleDing)
+	http.HandleFunc("/post2ding", handlePost2Ding)
 
+	log.Printf("serving...\n")
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 func handleDing(w http.ResponseWriter, r *http.Request) {
 	log.Printf("recv: %s\n", r.URL)
 	fmt.Fprintf(w, "recv: %s\n", r.URL)
+}
+
+func handlePost2Ding(w http.ResponseWriter, r *http.Request) {
+	url := "https://oapi.dingtalk.com/robot/send?access_token=a6b3039dc83a8911870274109dea4671286db8091f646aa44c7dd266bc0c579d"
+	contentType := "Content-Type: application/json"
+
+	text := make(map[string]string)
+	text["content"] = "test"
+	msg := make(map[string]interface{})
+	msg["msgtype"] = "text"
+	msg["text"] = text
+
+	out, err := json.Marshal(msg)
+	if err != nil {
+		fmt.Fprintf(w, "error: %s", err)
+	} else {
+		http.Post(url, contentType, bytes.NewReader(out))
+		fmt.Fprintf(w, "ok: %s", string(out))
+	}
 }
